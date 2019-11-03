6eced559fecb745cd001492aa8d43dff8e66dd18
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2019-11-03 20:46:58 +0000

    combined items in cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_03_204654) do
+ActiveRecord::Schema.define(version: 2019_11_03_204656) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
